4219b55b57294f404e050217992bf5c2384d5d6e
Halil İbrahim Özdoğan <hiozdogan2@gmail.com>
2022-07-12 12:15:47 +0200

    build command updated

--- src/config/endpoints.ts
+++ src/config/endpoints.ts
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+const BASE_URL = process.env.NODE_ENV === 'development'
   ? 'http://localhost:3333' : 'https://reservation-api.ibrahimozdogan.com';
 const ENDPOINTS = {
   SAVE_RESERVATION: `${BASE_URL}/v1/save-reservation`,
